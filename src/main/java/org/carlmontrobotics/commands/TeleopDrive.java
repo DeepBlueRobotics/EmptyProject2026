f94c3c6c56b8d0321460a6d1ae49232e912f1962
MollyChen11 <mollyyhchen@gmail.com>
2026-09-20 13:36:00 -0700

    feed forward command

--- src/main/java/org/carlmontrobotics/commands/TeleopDrive.java
+++ src/main/java/org/carlmontrobotics/commands/TeleopDrive.java
@@ -29,7 +29,7 @@ public class TeleopDrive extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.runMotorPID(200);
+    drivetrain.runMotorFeedforward(200);
   }
 
   // Called once the command ends or is interrupted.
